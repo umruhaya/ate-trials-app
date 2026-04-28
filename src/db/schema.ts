50b0c40cf900e28e54fbe88f928a4f00a639bb8e
Umer Naeem <umernaeem135acc@gmail.com>
2026-04-28 21:04:52 +0500

    attempt at completing data model

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,12 @@
 import { sql } from "drizzle-orm";
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import {
+	blob,
+	integer,
+	real,
+	sqliteTable,
+	text,
+} from "drizzle-orm/sqlite-core";
+import type { EventMeta, StructuredAnnotation } from "~/schemas";
 
 export const users = sqliteTable("users", {
 	id: text("id").primaryKey(),
@@ -7,5 +14,99 @@ export const users = sqliteTable("users", {
 	role: text({ enum: ["admin", "data-annotator"] }).notNull(),
 	createdAt: integer("created_at", { mode: "timestamp" })
 		.default(sql`(unixepoch())`)
+		.notNull(),
+});
+
+export const communityPortals = sqliteTable("community_portals", {
+	id: text("id").primaryKey(),
+	name: text().notNull(),
+	logo: blob({ mode: "buffer" }),
+	slug: text().notNull().unique(),
+	description: text().notNull(),
+	createdBy: text("created_by")
+		.references(() => users.username)
+		.notNull(),
+	isActive: integer("enabled", { mode: "boolean" }).notNull().default(true),
+	createdAt: integer("created_at", { mode: "timestamp" })
+		.default(sql`(unixepoch())`)
+		.notNull(),
+});
+
+export const deployments = sqliteTable("deployments", {
+	id: text("id").primaryKey(),
+
+	// Macro Geography
+	city: text("city").notNull(),
+	state: text("state").notNull(),
+	countryCode: text("country_code").notNull(),
+
+	// Micro Geography
+	latitude: real("latitude").notNull(),
+	longitude: real("longitude").notNull(),
+	locationName: text("location_name").notNull(), // e.g., "Intersection of 5th and Main"
+	directionFacing: text("direction_facing", {
+		enum: ["N", "S", "E", "W", "NE", "NW", "SE", "SW"],
+	}).notNull(),
+	zoneType: text("zone_type", {
+		enum: ["intersection", "crosswalk", "mid-block", "school-zone"],
+	}).notNull(),
+
+	// Infrastructure
+	mountingPoint: text("mounting_point", {
+		enum: [
+			"mast-arm",
+			"streetlight",
+			"utility-pole",
+			"pedestrian-beacon",
+			"mobile-trailer",
+		],
+	}).notNull(),
+
+	// Hardware Context
+	deviceId: text("device_id").notNull(),
+});
+
+export const trials = sqliteTable("trials", {
+	id: text("id").primaryKey(),
+	title: text().notNull(),
+	description: text().notNull(),
+	createdBy: text("created_by")
+		.references(() => users.username)
+		.notNull(),
+	isActive: integer("enabled", { mode: "boolean" }).notNull().default(true),
+	communityPortalId: text("community_portal_id")
+		.references(() => communityPortals.id)
+		.notNull(),
+	createdAt: integer("created_at", { mode: "timestamp" })
+		.default(sql`(unixepoch())`)
+		.notNull(),
+});
+
+export const events = sqliteTable("events", {
+	id: text("id").primaryKey(),
+	// reference to external blob storage for raw video data
+	externalBlobRef: text().notNull(),
+	// timestamp of the event
+	timestamp: integer("timestamp", { mode: "timestamp" }).notNull(), // Date
+	deploymentId: text("deployment_id")
+		.references(() => deployments.id)
+		.notNull(),
+	metadata: text("metadata", { mode: "json" }).$type<EventMeta>().notNull(),
+});
+
+export const structuredAnnotations = sqliteTable("structured_annotations", {
+	id: text("id").primaryKey(),
+	eventId: text("event_id")
+		.references(() => events.id)
+		.notNull(),
+	isViolation: integer("is_violation", { mode: "boolean" }).notNull(),
+	annotation: text("annotation", { mode: "json" })
+		.$type<StructuredAnnotation>()
+		.notNull(),
+	createdAt: integer("created_at", { mode: "timestamp" })
+		.default(sql`(unixepoch())`)
+		.notNull(),
+	createdBy: text("created_by")
+		.references(() => users.username)
 		.notNull(),
 });
---
+++ src/schemas.ts
@@ -0,0 +1,70 @@
+import { z } from "zod";
+
+// We Assume that this data is available at ingestion time (from the edge models)
+export const EventMetaSchema = z.object({
+	vehicleSpeedInMilesPerHour: z.number().optional(),
+	detectedLicensePlate: z.string().optional(),
+	videoDurationInMs: z.number(),
+});
+
+export type EventMeta = z.infer<typeof EventMetaSchema>;
+
+const annotationData = z.object({
+	videoTimestampInMs: z.number(),
+	boundingBox: z.object({
+		x: z.number(),
+		y: z.number(),
+		width: z.number(),
+		height: z.number(),
+	}),
+});
+
+const noViolation = z.object({ type: z.literal("no-violation") });
+
+const failureToYield = z.object({
+	type: z.literal("failure-to-yield"),
+	vehicleAnnotation: annotationData,
+	pedestrianAnnotation: annotationData,
+});
+
+const redLightViolation = z.object({
+	type: z.literal("red-light-violation"),
+	vehicleAnnotation: annotationData,
+	redLightAnnotation: annotationData,
+});
+
+const speedingViolation = z.object({
+	type: z.literal("speeding-violation"),
+	vehicleAnnotation: annotationData,
+	speedLimit: z.number(),
+	actualSpeed: z.number(),
+});
+
+const distractedDrivingViolation = z.object({
+	type: z.literal("distracted-driving-violation"),
+	vehicleAnnotation: annotationData,
+});
+
+const stopSignViolation = z.object({
+	type: z.literal("stop-sign-violation"),
+	vehicleAnnotation: annotationData,
+});
+
+const seatbeltViolation = z.object({
+	type: z.literal("seatbelt-violation"),
+	vehicleAnnotation: annotationData,
+});
+
+export const StructuredAnnotationSchema = z.object({
+	violation: z.union([
+		noViolation,
+		failureToYield,
+		redLightViolation,
+		speedingViolation,
+		distractedDrivingViolation,
+		stopSignViolation,
+		seatbeltViolation,
+	]),
+});
+
+export type StructuredAnnotation = z.infer<typeof StructuredAnnotationSchema>;
